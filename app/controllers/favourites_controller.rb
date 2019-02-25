33bd8bc614ab889465ea89902107ae0467ddd4ec
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-25 15:32:57 -0500

    Finished the favorites controller and the review controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,7 +1,24 @@
 class FavouritesController < ApplicationController
   def create
+    @favorite = Favorite.new(favorite_params)
+    @hike = Hike.find(params[:hike_id])
+    @favorite.hike = @hike
+    if @favorite.save
+      redirect_to hike_path(@hike)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    redirect_to hike_path(@favorite.hike)
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :hike_id)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,42 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save!
+      redirect_to review_path
+    else
+      render :new
+    end
   end
 
   def index
+    @reviews = Review.all
   end
 
   def show
+    @reviews = Review.find(params[:id])
   end
 
   def update
+    @review.update(params[:review])
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def destroy
+    @review.destroy
+    redirect_to hike_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:stars, :user_id, :hike_id, :content, :integer)
   end
 end
