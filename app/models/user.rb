51bdcc61476e587feb0c680da1afc6ee1cee50e6
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-25 14:34:33 -0500

    Added dotenv to protect information

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :favourites, :reviews
+  has_many :favourites
+  has_many :reviews
   validates :address, :first_name, :last_name, :photo, presence: true
 end
