c1a97605fcaec2223adeae526556de061ce44767
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-26 13:49:11 -0500

    Map working on Hike Index Page

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,6 +1,7 @@
 class Hike < ApplicationRecord
   has_many :reviews
   has_many :favourites
+  validates :city, presence: true
 
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
