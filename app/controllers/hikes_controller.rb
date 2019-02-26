5394d3d9dba82227258f0c4766e043573c0bf92e
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-26 15:31:41 -0500

    Filtered by distance and city, and added to the searchbar

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -3,15 +3,15 @@ class HikesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @hikes = Hike.where.not(latitude: nil, longitude: nil)
     if params[:query].present? || params[:category].present? || params[:city].present?
       hike_query = "name ILIKE :query OR description ILIKE :query"
       @hikes = Hike.where(hike_query, query: "%#{params[:query]}%")
       @hikes = @hikes.where("category ILIKE :category", category: "%#{params[:category]}%")
-      @hikes = Hike.near(params[:city], params[:distance] || 10, order: :distance)
+      @hikes = Hike.near(params[:city], params[:distance] || 10)
     else
       @hikes = Hike.all
     end
-    @hikes = Hike.where.not(latitude: nil, longitude: nil)
     @markers = @hikes.map do |hike|
       {
         lat: hike.latitude,
@@ -26,7 +26,7 @@ class HikesController < ApplicationController
   private
 
   def hike_params
-    params.require(:hike).permit(:query, :photo, :city, :category, :categories_attributes => :name)
+    params.require(:hike).permit(:query, :distance, :photo, :city, :category, :categories_attributes => :name)
   end
 
   def set_hike
