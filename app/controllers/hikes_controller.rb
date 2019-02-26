9e8b5f91761d3be4ad4401d026d1589c2531feac
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-26 10:15:36 -0500

    Search bar working

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,18 +1,27 @@
 class HikesController < ApplicationController
-    before_action :set_hike, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_hike, only: [:show]
 
   def index
-    @hikes = Hike.all
+    if params[:query].present? || params[:category].present?
+      hike_query = "name ILIKE :query OR description ILIKE :query"
+      @hikes = Hike.where(hike_query, query: "%#{params[:query]}%")
+      @hikes = @hikes.where("category ILIKE :category", category: "%#{params[:category]}%")
+    else
+      @hikes = Hike.all
+    end
   end
 
   def show
-
   end
 
   private
 
+  def hike_params
+    params.require(:hike).permit(:query, :photo, :city, :category, :categories_attributes => :name)
+  end
+
   def set_hike
     @hike = Hike.find(params[:hike_id])
   end
-
 end
