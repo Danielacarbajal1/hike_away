08618a1633915b71f32d37891807e5e876312934
Gracy Soundron <gracesoundron548@gmail.com>
2019-02-26 15:51:11 -0500

    user and review done in seed

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -3,7 +3,6 @@ class HikesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-
     if params[:query].present? || params[:category].present?
       hike_query = "name ILIKE :query OR description ILIKE :query"
       @hikes = Hike.where(hike_query, query: "%#{params[:query]}%")
@@ -20,7 +19,6 @@ class HikesController < ApplicationController
         lng: hike.longitude
       }
     end
-
   end
 
   def show
