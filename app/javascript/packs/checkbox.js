d14ae4d9e6f728701664123d8d374487b66d3b89
Daniella Depaz <danielladcoding@protonmail.com>
2019-03-07 14:16:10 -0500

    Stuff

--- app/javascript/packs/checkbox.js
+++ app/javascript/packs/checkbox.js
@@ -118,7 +118,6 @@ sunscreen.addEventListener('click', function () {
     count = count + (100 / 7)
     console.log("Your hat is checked")
 
-
   } else {
     // var dontProgresshat = document.getElementById("dontProgress")
     count = count - (100 / 7)
